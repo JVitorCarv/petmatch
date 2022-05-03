6557f9385bfc31ee91baf0ac6d5633873860fb15
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-05-03 17:51:07 -0300

    Add random pk to profile

--- petmatch/urls.py
+++ petmatch/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('login/', views.login),
     path('register/', views.register),
     path('feed/', views.feed),
-    path('profile/', views.profile),
+    path('profile/<int:id>/', views.profile),
 ]
