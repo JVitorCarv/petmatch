979d7294d61ee7c0a454f7f92e76984905b418f9
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-05-05 16:51:16 -0300

    Add redirect in case you already have account

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractBaseUser
 
 # Create your models here.
+class AbstractUser(AbstractBaseUser):
+    username = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(blank=True)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
