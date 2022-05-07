e06f0a5a6b4ebb842afa6c607f5e97b8ab55a025
Dioguera <dcmr@cesar.school>
2022-05-07 18:04:32 -0300

    add signup test

--- petmatchaut/tests.py
+++ petmatchaut/tests.py
@@ -1,8 +1,10 @@
 from threading import Thread
 from time import sleep
+from webbrowser import Chrome
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 import os
 
 class TestHome(LiveServerTestCase):
@@ -31,16 +33,20 @@ class TestSignup(LiveServerTestCase):
         
         #populate the form with data
         email.send_keys('selenium.test@gmail.com')
-        username.send_keys('Selenium')
+        username.send_keys('Selen')
         first_name.send_keys('Sele')
         last_name.send_keys('Nium')
         password.send_keys('momentanio')
         password_confirm.send_keys('momentanio')
-        Thread(sleep(3))
 
-        signup.find_elements_by_xpath("//button[contains(@class,'btn btn-sucess')]")
-
+        signup.find_element_by_class_name("btn-success").click()
+        Thread(sleep(5))
         
+        
+        
+        
+
+'''   
 class TestLogin(LiveServerTestCase):
     def testLogin(self):
         login = webdriver.Chrome()
@@ -53,14 +59,23 @@ class TestLogin(LiveServerTestCase):
         password.send_keys('momentanio')
         Thread(sleep(3))
 
-        login.find_elements_by_xpath("//button[contains(@class,'btn btn-sucess')]")
+        submit login.find_elements_by_xpath("//button[contains(@class,'btn btn-sucess')]")
 
 
 class testAddPet(LiveServerTestCase):
     def testAddPet(self):
         login = webdriver.Chrome()
-        login.get('http://127.0.0.1:8000/add_pet/')
+        login.get('http://127.0.0.1:8000/accounts/login/')
+
+        email = login.find_element_by_id('id_login')
+        password = login.find_element_by_id('id_password')
+
+        email.send_keys('selenium@gmail.com')
+        password.send_keys('momentanio')
+        Thread(sleep(3))
         
+        login.find_elements_by_xpath("//button[contains(@class,'btn btn-sucess')]")
+
         pet_name = login.find_element_by_id('id_pet_name')
         pet_race = login.find_element_by_id('id_race')
         pet_age = login.find_element_by_id('id_pet_age')
@@ -69,7 +84,8 @@ class testAddPet(LiveServerTestCase):
         pet_name.send_keys('Pet')
         pet_race.send_keys('Race')
         pet_age.send_keys('5')
-        pet_image.send_keys(os.getcwd()+"/cao.png")
+        pet_image.send_keys(os.getcwd()+"media/cao.png")
         Thread(sleep(3))
 
         login.find_elements_by_xpath("//button[contains(@class,'btn btn-sucess')]")
+'''
